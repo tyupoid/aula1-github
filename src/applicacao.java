abfbc578f6feaf70a4cf7f3c095619eb897289f1
Thiago Service <thiago_tyupoid@hotmail.com>
2019-02-11 12:09:27 -0200

    Ola

--- src/applicacao.java
+++ src/applicacao.java
@@ -2,7 +2,8 @@
 public class applicacao {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println(" Hello World");
+		System.out.println(" Bye");
 	}
 
 }
