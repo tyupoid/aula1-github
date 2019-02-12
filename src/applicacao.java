161373a52f993a4763e865482cf22865268be6b4
Thiago Service <thiago_tyupoid@hotmail.com>
2019-02-12 13:13:19 -0200

    2parte

--- src/applicacao.java
+++ src/applicacao.java
@@ -1,9 +1,42 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
+import entities.Departament;
+import entities.Worker;
+import entities.enums.WorkerLevel;
 
 public class applicacao {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat stf = new SimpleDateFormat("dd/mm/yyyy");
+
+		System.out.print("Enter Departament's name: ");
+		String departamentName = sc.nextLine();
+		System.out.println("Enter Worker data: ");
+		System.out.println("Name: ");
+		String workerName = sc.nextLine();
+		System.out.println("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.println("BaseSalary: ");
+		double baseSalary = sc.nextDouble();
+
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary,
+				new Departament(departamentName));
+
+		System.out.println("How many conctracts to this worker?");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter contract #" + i + "+Data:");
+			System.out.print("Date  (DD/MM/YYYY)");
+			Date contractDate =  stf.parse(sc.next());
+
+		}
 	}
 
 }
--- src/entities/enums/WorkerLevel.java
+++ src/entities/enums/WorkerLevel.java
@@ -1,5 +1,7 @@
 package entities.enums;
 
 public enum WorkerLevel {
-
+	JUNIOR,
+	MID_LEVEL, 
+	SENIOR,
 }
