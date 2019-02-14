29d8d547ba0d90dea501a80bd9ab3cdf0a5a85c5
Thiago Service <thiago_tyupoid@hotmail.com>
2019-02-14 18:10:45 -0200

    Abstrata

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,59 +1,23 @@
-package Application;
+package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
-
-import entities.ImportedProduct;
-import entities.Product;
-import entities.UsedProduct;
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
-		List<Product> list = new ArrayList<>();
+	public static void main(String[] args) {
 
-		System.out.print("Enter the number of products: ");
-		int n = sc.nextInt();
-
-		for (int i = 1; i  <=n; i++) {
-			System.out.println("Product #" + i + " data:");
-			System.out.print("Common,used or imported (c/u/i)?");
-			char ch = sc.next().charAt(0);
-			System.out.print("Name: ");
-			sc.nextLine();
-			String name = sc.nextLine();
-			System.out.print("Price: ");
-			double price = sc.nextDouble();
-
-			if (ch == 'i') {
-				System.out.print("Customs Fee: ");
-				double customsFee = sc.nextDouble();
-				list.add(new ImportedProduct(name, price, customsFee));
-			} else if (ch == 'c') {
-				list.add(new Product(name, price));
-			} else {
-				System.out.print("Manufacture date: ");
-				Date manufactureDate = sdf.parse(sc.next());
-				list.add(new UsedProduct(name, price,manufactureDate));
-			}
-		}
-
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getBalance());
 		
-		System.out.println("PRICE TAGS: ");
-		for (Product product : list) {
-			System.out.println(product.priceTag());
-		}
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
 		
-		sc.close();
-
+		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		acc3.withdraw(200.0);
+		System.out.println(acc3.getBalance());
 	}
-
 }
