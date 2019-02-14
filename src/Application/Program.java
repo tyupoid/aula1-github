3508e24935031319d38e45f05b3f778ea2cea093
Thiago Service <thiago_tyupoid@hotmail.com>
2019-02-14 18:00:16 -0200

    ExercicioHeranca

---
+++ src/Application/Program.java
@@ -0,0 +1,59 @@
+package Application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		List<Product> list = new ArrayList<>();
+
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i  <=n; i++) {
+			System.out.println("Product #" + i + " data:");
+			System.out.print("Common,used or imported (c/u/i)?");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+
+			if (ch == 'i') {
+				System.out.print("Customs Fee: ");
+				double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+			} else if (ch == 'c') {
+				list.add(new Product(name, price));
+			} else {
+				System.out.print("Manufacture date: ");
+				Date manufactureDate = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price,manufactureDate));
+			}
+		}
+
+		
+		System.out.println("PRICE TAGS: ");
+		for (Product product : list) {
+			System.out.println(product.priceTag());
+		}
+		
+		sc.close();
+
+	}
+
+}
